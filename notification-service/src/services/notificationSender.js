73da87c3ef97125d801b189e801c423c66e3feb9
Himanshu S Gautam <2024tm93048@wilp.bits-pilani.ac.in>
2025-11-08 22:29:32 +0100

    Notification Service Removed

--- notification-service/src/services/notificationSender.js
+++
@@ -1,9 +0,0 @@
-import db from "../db/connection.js";
-
-export const sendNotification = async (eventType, accountId, amount, message) => {
-  await db.query(
-    `INSERT INTO notifications (event_type, account_id, amount, message)
-     VALUES ($1, $2, $3, $4)`,
-    [eventType, accountId, amount, message]
-  );
-};
